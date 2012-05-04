a99a9c9cc3b90a2907161c5be7e3083d0ddaeb31
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-04 11:05:37 +0200

    use nonfree recipe in java recipe

--- java/recipes/default.rb
+++ java/recipes/default.rb
@@ -1,18 +1,7 @@
-template "/etc/apt/sources.list.d/java.list" do
-  source "java.list"
-  owner "root"
-  mode "644"
-  notifies :run, "execute[apt-get-update]", :immediately
-end
+include_recipe "nonfree"
 
 execute "agree to Sun license" do
   command 'echo "sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true" | debconf-set-selections'
-
-end
-
-execute "apt-get-update" do
-  command "apt-get update"
-  action :nothing
 end
 
 package "sun-java6-jre"
