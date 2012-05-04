37055ff808f45c8f92bdcae09c5a46f7814fd6ac
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-04 12:45:00 +0200

    split solr master and slave (start script, home, etc)

--- solr/recipes/default.rb
+++ solr/recipes/default.rb
@@ -35,30 +35,53 @@ link "#{INSTALL_DIR}/apache-solr" do
   to SOLR_DIR
 end
 
-directory SOLR_DATA_DIR do
-  action :create
-  owner SOLR_USER
-  recursive true
-end
-
-directory "#{SOLR_DATA_DIR}/conf" do
-  action :create
-  owner SOLR_USER
-  recursive true
-end
-
-template "/etc/init.d/solr-jetty" do
+template "/etc/init.d/solr_master" do
   source "solr-jetty.erb"
   mode "0744"
+  variables(
+    :solr_home => "#{SOLR_DATA_DIR}/master",
+    :solr_port => @node.solr.master_port,
+    :role => "master",
+    :master_slave_options => "-Denable.master=true"
+  )
 end
 
-template "/data/solr/solr.xml" do
-  action :create_if_missing
-  source "solr.xml"
+template "/etc/init.d/solr_slave" do
+  source "solr-jetty.erb"
   mode "0744"
-  owner SOLR_USER
+  variables(
+    :solr_home => "#{SOLR_DATA_DIR}/slave",
+    :solr_port => @node.solr.slave_port,
+    :role => "slave",
+    :master_slave_options => "-Denable.slave=true -Dmaster.url=#{@node.solr.master_url}"
+  )
 end
 
-execute "cp -Rv #{File.expand_path("../../files/conf", __FILE__)} #{SOLR_DATA_DIR} && chown #{SOLR_USER} #{SOLR_DATA_DIR}" do
-  creates "#{SOLR_DATA_DIR}/conf/schema.xml"
+["#{SOLR_DATA_DIR}/master", "#{SOLR_DATA_DIR}/slave"].each do |solr_home|
+  directory solr_home do
+    action :create
+    owner SOLR_USER
+    recursive true
+  end
+
+  directory "#{solr_home}/conf" do
+    action :create
+    owner SOLR_USER
+    recursive true
+  end
+
+  template "#{solr_home}/solr.xml" do
+    action :create_if_missing
+    source "solr.xml"
+    mode "0744"
+    owner SOLR_USER
+    variables(
+      :solr_home => solr_home
+    )
+  end
+
+  execute "init solr home" do
+    command "cp -Rv #{File.expand_path("../../files/conf", __FILE__)} #{solr_home} && chown #{SOLR_USER} #{solr_home}"
+    creates "#{solr_home}/conf/schema.xml"
+  end
 end
