02a1e6cb59ebf7957b625009cf07aa0ab279bcea
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-04 13:15:49 +0200

    remove all testing stuff

--- tests/provision.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/var/lib/gems/1.8/bin:$PATH
-test -f /usr/bin/gem || sudo apt-get -y install ruby rubygems
-test -f /var/lib/gems/1.8/bin/chef-solo || sudo gem install chef --no-ri --no-rdoc
-
-chef-solo -c /vagrant/config.rb -j /vagrant/tests/dna.json
--- tests/run.rb
+++
@@ -1,8 +0,0 @@
-if package = ARGV.first
-  puts "running tests for #{package}"
-  ENV["PATH"] = "/var/lib/gems/1.8/bin:#{ENV["PATH"]}"
-  system %(test -f /usr/bin/gem || sudo apt-get -y install ruby rubygems)
-  system %(test -f /var/lib/gems/1.8/bin/chef-solo || sudo gem install chef --no-ri --no-rdoc)
-  
-  exec "chef-solo -c /vagrant/config.rb -j /vagrant/tests/#{package}.json"
-end
