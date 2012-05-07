99749c18b9a961aef3fea335f9d2e7c114d6aea4
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-07 15:19:55 +0200

    install rvm in user home again

--- rvm/attributes/default.rb
+++ rvm/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:rvm][:version] = "1.13.4"
+default[:rvm][:user] = "rvm"
--- rvm/recipes/default.rb
+++ rvm/recipes/default.rb
@@ -1,27 +1,35 @@
 require "pathname"
-INSTALL_DIR = "/opt"
-SRC_DIR = "#{INSTALL_DIR}/src"
+USER = node[:user] || node.rvm.user
+USER_HOME = "/home/#{USER}"
+SRC_DIR = "#{USER_HOME}/src"
 RVM_VERSION = node.rvm.version
-RVM_DIR = "/opt/rvm-#{RVM_VERSION}"
+RVM_DIR = "#{USER_HOME}/.rvm-#{RVM_VERSION}"
 
-# libxml2-dev libmysqlclient-dev git-core libxslt1-dev
 %w(build-essential openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev).each do |package_name|
   package package_name do
     action :install
   end
 end
 
+user USER
+directory USER_HOME do
+  owner USER
+end
+
 directory SRC_DIR do
   action :create
+  owner USER
   recursive true
 end
 
 remote_file "#{SRC_DIR}/rvm-installer" do
   source "https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer"
   mode "744"
+  owner USER
 end
 
 bash "install rvm" do
+  user USER
   environment("rvm_path" => RVM_DIR)
   code %(bash #{SRC_DIR}/rvm-installer --version #{RVM_VERSION})
   creates "#{RVM_DIR}/scripts/rvm"
@@ -39,8 +47,10 @@ def install_rvm_ruby(version)
   end
 end
 
-link "#{INSTALL_DIR}/rvm" do
+link "#{USER_HOME}/.rvm" do
   to RVM_DIR
+  not_if "test -d #{USER_HOME}/.rvm && test ! -L #{USER_HOME}/.rvm"
+  owner USER
 end
 
 if node[:rvm] && node[:rvm][:rubies]
