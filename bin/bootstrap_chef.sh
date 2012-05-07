78fbc0948bbe9719889b3e88e6497c8cf26adebb
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-07 14:28:38 +0200

    add cloning of cookbooks to bootstrap script

--- bin/bootstrap_chef.sh
+++ bin/bootstrap_chef.sh
@@ -1,3 +1,8 @@
 export PATH=/var/lib/gems/1.8/bin:$PATH
-test -f /usr/bin/gem || sudo apt-get -y install ruby rubygems
-test -f /var/lib/gems/1.8/bin/chef-solo || sudo gem install chef --no-ri --no-rdoc
+
+test -f /usr/bin/git || apt-get -y install git-core
+test -f /usr/bin/gem || apt-get -y install ruby rubygems
+test -f /var/lib/gems/1.8/bin/chef-solo || gem install chef --no-ri --no-rdoc
+test -d /root/cookbooks || git clone https://github.com/dynport/cookbooks.git /root/cookbooks
+cd /root/cookbooks && git pull --rebase origin
+cd /root
