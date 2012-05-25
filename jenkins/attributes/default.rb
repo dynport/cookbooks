083ca92c343d8273f9cb90746c49a9fb57e07614
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-25 20:26:37 +0200

    add default for plugins attribute

--- jenkins/attributes/default.rb
+++ jenkins/attributes/default.rb
@@ -2,3 +2,4 @@ default[:jenkins][:version] = "1.465"
 default[:jenkins][:port]    = "8080"
 default[:jenkins][:address] = "127.0.0.1"
 default[:jenkins][:user] = "jenkins"
+default[:jenkins][:plugins] = []
