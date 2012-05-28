046761f4c73a2b6a873c96b340ec891962d1702a
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-28 23:02:59 +0200

    fix graphite installation because of hardcoded paths

--- graphite/recipes/default.rb
+++ graphite/recipes/default.rb
@@ -35,29 +35,25 @@ end
 
 execute "install whisper" do
   cwd "/tmp"
-  command "tar xvfz #{src_dir}/#{WHISPER_FILE} && cd /tmp/#{WHISPER_PREFIX} && python setup.py install --prefix #{GRAPHITE_DIR}"
-  creates "#{GRAPHITE_DIR}/bin/whisper-fetch.py"
-  user USER
+  command "tar xvfz #{src_dir}/#{WHISPER_FILE} && cd /tmp/#{WHISPER_PREFIX} && python setup.py install"
+  creates "/usr/local/bin/whisper-fetch.py"
 end
 
 execute "install carbon" do
   cwd "/tmp"
-  command "tar xvfz #{src_dir}/#{CARBON_FILE} && cd /tmp/#{CARBON_PREFIX} && python setup.py install --prefix #{GRAPHITE_DIR}"
+  command "tar xvfz #{src_dir}/#{CARBON_FILE} && cd /tmp/#{CARBON_PREFIX} && python setup.py install"
   creates "#{GRAPHITE_DIR}/bin/carbon-client.py"
-  user USER
 end
 
 execute "unpack graphite-web" do
   cwd "/tmp"
-  command "tar xvfz #{src_dir}/#{GRAPHITE_FILE} && cd /tmp/#{GRAPHITE_PREFIX} && python setup.py install --prefix #{GRAPHITE_DIR}"
+  command "tar xvfz #{src_dir}/#{GRAPHITE_FILE} && cd /tmp/#{GRAPHITE_PREFIX} && python setup.py install"
   creates "#{GRAPHITE_DIR}/conf/graphite.wsgi.example"
-  user USER
 end
 
 execute "create django database" do
   cwd DJANGO_ROOT
   command "python ./manage.py syncdb --noinput"
-  user USER
   creates "#{GRAPHITE_DIR}/storage/graphite.db"
 end
 
@@ -71,26 +67,26 @@ end
 execute "symlink whisper" do
   command "rmdir #{GRAPHITE_DIR}/storage/whisper; ln -nfs #{WHISPER_DATA_DIR} #{GRAPHITE_DIR}/storage/whisper"
   not_if "readlink #{GRAPHITE_DIR}/storage/whisper | grep #{WHISPER_DATA_DIR}"
-  user USER
 end
 
 template "#{GRAPHITE_DIR}/conf/carbon.conf" do
-  owner USER
   mode "644"
 end
 
 template "#{GRAPHITE_DIR}/conf/storage-aggregation.conf" do
-  owner USER
   mode "644"
 end
 
 template "#{GRAPHITE_DIR}/conf/storage-schemas.conf" do
-  owner USER
   mode "644"
 end
 
 template "/etc/init.d/graphite" do
   mode "755"
+end
+
+execute "chown /opt/graphite" do
+  command "chown -R #{USER}:#{USER} #{GRAPHITE_DIR}"
 end
 
 nginx_unix_proxy(:name => "graphite", :socket_path => "/tmp/graphite.socket", :root => DJANGO_ROOT, 
