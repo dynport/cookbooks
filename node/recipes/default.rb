a00b4252d0b7b58dfe725d9f27aa8156b049d4f9
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-28 23:11:04 +0200

    add node.js recipe

---
+++ node/recipes/default.rb
@@ -0,0 +1,13 @@
+include_recipe "source"
+
+NODE_VERSION = "0.6.18"
+NODE_PREFIX = "node-v#{NODE_VERSION}"
+NODE_FILE = "#{NODE_PREFIX}.tar.gz"
+
+download_file "http://nodejs.org/dist/v#{NODE_VERSION}/#{NODE_FILE}"
+
+execute "install node" do
+  command "tar xvfz #{NODE_FILE} && cd #{NODE_PREFIX} && ./configure --prefix=/opt/#{NODE_PREFIX} && make && make install"
+  cwd src_dir
+  creates "/opt/#{NODE_PREFIX}/bin/node"
+end
