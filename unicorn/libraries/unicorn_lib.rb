17be8a1d70dcf1d9d7c41365f77adc7b4806a882
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-29 19:56:45 +0200

    able to provide -l to unicorn start/stop script

--- unicorn/libraries/unicorn_lib.rb
+++ unicorn/libraries/unicorn_lib.rb
@@ -7,6 +7,7 @@ def unicorn_start_stop_script(attributes)
   attributes[:environment] ||= "production"
   daemon_args = "unicorn -E #{attributes[:environment]}"
   daemon_args << " -c #{attributes[:unicorn_config]}" if attributes[:unicorn_config]
+  daemon_args << " -l #{attributes[:listen]}" if attributes[:listen]
   attributes[:start_stop_env] = "environment_id=#{attributes[:rvm_env]}"
   start_stop_script(attributes.merge(:pidfile => "/tmp/unicorn_#{name}.pid", :daemon => "#{node.source.install_dir}/bundle_wrapper", :daemon_args => daemon_args))
 end
