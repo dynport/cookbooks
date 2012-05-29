c3e3d70b8ea548876906e17126197712b073bccf
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-29 11:41:27 +0200

    add comments for accepted parameters for start_stop_script

--- base/libraries/base_lib.rb
+++ base/libraries/base_lib.rb
@@ -1,3 +1,12 @@
+# name           : name of the daemon
+# user           : user to be used to run daemon as
+# cwd            : used to chdir before starting daemon
+# pidfile        : path to pidfile used by start-stop-daemon
+# daemon         : pass to script used to run daemon
+# daemon_args    : arguments passed after -- 
+# create_pidfile : tells start-stop-daemon to create the pidfile (using -m)
+# syslog_flag    : pipes output of daemon to | logger -t <syslog_flag> when set
+
 def start_stop_script(attributes)
   [:user, :cwd, :name, :pidfile, :daemon].each do |key|
     raise "#{key} not set" if !attributes.has_key?(key)
