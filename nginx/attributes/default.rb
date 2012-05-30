af152884f4df9468648872852c2e0a911df60e69
Tobias Schwab <tobias.schwab@dynport.de>
2012-05-30 12:46:58 +0200

    able to bind nginx to a specific port and interface

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -3,3 +3,5 @@ default[:nginx][:version]       = "1.2.0"
 default[:nginx][:install_root]  = "/opt"
 default[:nginx][:dir]           = "#{default[:nginx][:install_root]}/nginx"
 default[:nginx][:processes]     = 1
+default[:nginx][:address]       = "127.0.0.1"
+default[:nginx][:port]          = 80
