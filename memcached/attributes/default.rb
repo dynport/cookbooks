e21384ad8ae08cdf91d715dfc3f7ab93f22a5ae7
Tobias Schwab <tobias.schwab@dynport.de>
2012-07-09 13:17:05 +0200

    able to specifiy number of mg to use for memcached

--- memcached/attributes/default.rb
+++ memcached/attributes/default.rb
@@ -1,3 +1,4 @@
 default["memcached"]["version"] = "1.4.13"
 default["memcached"]["user"] = "memcached"
 default["memcached"]["checksum"] = "cb0b8b87aa57890d2327906a11f2f1b61b8d870c0885b54c61ca46f954f27e29"
+default["memcached"]["memory"] = "64"
