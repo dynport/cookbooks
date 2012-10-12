3cc04f2e0f58534ed0a149191b04f3b9a6a03243
Tobias Schwab <tobias.schwab@dynport.de>
2012-10-12 18:50:44 +0200

    change most namings from mysql to percona

--- percona/recipes/server.rb
+++ percona/recipes/server.rb
@@ -7,10 +7,10 @@ name = file_name.gsub(".tar.gz", "")
 
 download_file url
 
-group "mysql"
+group "percona"
 
-user "mysql" do
-  group "mysql"
+user "percona" do
+  group "percona"
 end
 
 execute "install percona server" do
@@ -26,29 +26,35 @@ end
 data_dir = node.mysql.datadir
 
 directory data_dir do
-  owner "mysql"
+  owner "percona"
   mode "0755"
   recursive true
 end
 
-directory "/etc/mysql" do
+directory "/etc/percona-server" do
   mode "0755"
-  owner "mysql"
+  owner "percona"
 end
 
-template "/etc/mysql/my.cnf" do
+defaults_file = "/etc/percona-server/my.cnf"
+
+template defaults_file do
   mode "0644"
-  owner "mysql"
+  owner "percona"
 end
 
-template "/etc/init.d/mysql" do
-  source "mysql_start_stop"
+template "/etc/init.d/percona-server" do
+  source "percona_start_stop.erb"
   mode "0755"
+  variables(:defaults_file => defaults_file)
 end
 
 execute "mysql_install_db" do
   cwd "/opt/percona"
-  command "./scripts/mysql_install_db"
+  command "./scripts/mysql_install_db --defaults-file=#{defaults_file}"
   not_if "test -e #{data_dir}/mysql"
 end
 
+link "/root/.my.cnf" do
+  to defaults_file
+end
