01074a7f13316962921d1695a791b594bd2ff431
Tobias Schwab <tobias.schwab@dynport.de>
2012-11-06 12:02:08 +0100

    run redis as configured user

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,3 +1,4 @@
-default["redis"]["version"] = "2.4.12"
-default["redis"]["user"] = "redis"
-default["redis"]["checksum"] = "1947ecec21a1bd3216808d40f93d276e54fc65c239dedf97fc2c646c23f08511"
+default[:redis][:version]   = "2.4.12"
+default[:redis][:user]      = "redis"
+default[:redis][:group]     = "redis"
+default[:redis][:checksum]  = "1947ecec21a1bd3216808d40f93d276e54fc65c239dedf97fc2c646c23f08511"
