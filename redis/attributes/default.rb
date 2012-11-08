6efcca5612cd17349b75a6de5cff4a6199478c28
Tobias Schwab <tobias.schwab@dynport.de>
2012-11-08 13:45:24 +0100

    enable appendonly with config

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -2,4 +2,5 @@ default[:redis][:version]   = "2.4.12"
 default[:redis][:user]      = "redis"
 default[:redis][:group]     = "redis"
 default[:redis][:databases] = 16
+default[:redis][:aof]       = false
 default[:redis][:checksum]  = "1947ecec21a1bd3216808d40f93d276e54fc65c239dedf97fc2c646c23f08511"
