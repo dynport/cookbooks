055590cefa160b021f59d5fbc7e359709272b115
Tobias Schwab <tobias.schwab@dynport.de>
2012-11-30 17:54:27 +0100

    able to install ruby with library method

--- ruby/recipes/source.rb
+++ ruby/recipes/source.rb
@@ -1,16 +1,1 @@
-name = "ruby-#{node.ruby.version}"
-
-download_file "http://ftp.ruby-lang.org/pub/ruby/1.9/#{name}.tar.gz"
-%w(build-essential libyaml-dev libxml2-dev libxslt-dev libreadline-dev libssl-dev zlib1g-dev).each do |pkg|
-  package pkg
-end
-
-execute "install ruby-#{node.ruby.version}" do
-  cwd "/opt/src"
-  command "tar xvfz #{name}.tar.gz && cd #{name} && ./configure --prefix=/opt/#{name} && make && make install"
-  not_if "test -e /opt/#{name}/bin/ruby"
-end
-
-link "/opt/ruby" do
-  to "/opt/#{name}"
-end
+install_ruby(node.ruby.version)
