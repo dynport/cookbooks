e3870726e0a22265be632212b07f4a883f07c793
Tobias Schwab <tobias.schwab@dynport.de>
2012-12-03 11:29:23 +0100

    able to provide custom cflags to compile ruby, able to apply falcon patch

--- ruby/libraries/source.rb
+++ ruby/libraries/source.rb
@@ -1,4 +1,4 @@
-def install_ruby(version, symlink = true)
+def install_ruby(version, attributes = {})
   include_recipe "source"
   name = "ruby-#{version}"
 
@@ -7,13 +7,34 @@ def install_ruby(version, symlink = true)
     package pkg
   end
 
-  execute "install ruby-#{version}" do
-    cwd "/opt/src"
-    command "tar xvfz #{name}.tar.gz && cd #{name} && ./configure --prefix=/opt/#{name} && make && make install"
-    not_if "test -e /opt/#{name}/bin/ruby"
+  compile_env = {}
+  compile_env["CFLAGS"] = attributes[:cflags] if attributes[:cflags]
+
+  build_commands = []
+  build_commands << "tar xvfz #{name}.tar.gz && cd #{name}"
+
+  prefix = "/opt/#{name}"
+
+  # taken from https://gist.github.com/4136373
+  if attributes[:falcon_patch]
+    package "autoconf"
+    log "applying falcon patch"
+    file "/opt/src/falcon.diff" do
+      content File.read(File.expand_path("../../files/default/falcon.diff", __FILE__))
+    end
+    build_commands << "patch -p1</opt/src/falcon.diff"
+    prefix += "-falcon"
   end
-  
-  if symlink
+  build_commands << "./configure --prefix=#{prefix} && make && make install"
+
+  execute "install ruby-#{version}" do
+    env compile_env
+    cwd "/opt/src"
+    command build_commands.join(" && ")
+    not_if "test -e #{prefix}/bin/ruby"
+  end
+
+  if attributes[:symlink]
     link "/opt/ruby" do
       to "/opt/#{name}"
     end
